b159aab75833d241780c21091c9649b37f970e82
Brandon Andersen <31670449+bandersen23@users.noreply.github.com>
2020-07-21 10:26:09 -0400

    Update fibonacci.rs

--- fibonacci.rs
+++ fibonacci.rs
@@ -1,6 +1,9 @@
 fn fibonacci(n: u32) -> u32 {
-    if n<=1 {
+    if n==1 {
         1
+    }
+    else if n==0 {
+        0
     }
     else {
         fibonacci(n - 1) + fibonacci(n - 2)
@@ -8,6 +11,5 @@ fn fibonacci(n: u32) -> u32 {
 }
 
 fn main() {
-    // TODO: Not sure why this needs to be one number less to get same value as others.
-    println!("{}", fibonacci(39));
+    println!("{}", fibonacci(40));
 }
